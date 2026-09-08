5fb9e20d0fbba3485cdb022ac9b0808154081311
rohnshrma <rohn11shrma@gmail.com>
2026-09-08 18:04:59 +0530

    feat: initialize blog app with Express, EJS, and MongoDB
    - Created package.json with necessary dependencies and scripts.
    - Implemented server.js to set up Express server and routes for home, blogs, login, and registration.
    - Added views for login and registration with corresponding EJS templates.
    - Removed tasks.md as it is no longer needed.

---
+++ node sessions/blog-app/server.js
@@ -0,0 +1,92 @@
+import express from "express";
+import path from "node:path";
+import connectDB from "./config/db.js";
+import User from "./models/User.js";
+import { config } from "dotenv";
+import bcrypt from "bcryptjs";
+
+config();
+connectDB();
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+/* ---------- setup ---------- */
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+/* ---------- data (in memory for now) ---------- */
+const posts = [];
+const users = [];
+
+/* ---------- pages ---------- */
+app.get("/", (req, res) => {
+  res.render("home", { posts: posts.slice(-3).reverse() });
+});
+
+app.get("/blogs", (req, res) => {
+  res.render("blogs", { posts: [...posts].reverse() });
+});
+
+// app.get("/compose", (req, res) => {
+//   res.render("compose", { error: null });
+// });
+
+app.post("/compose", (req, res) => {
+  const { title, author, content } = req.body;
+  posts.push({ title, author, content, createdAt: new Date() });
+  res.redirect("/blogs");
+});
+
+/* ---------- auth ---------- */
+app.get("/login", (req, res) => {
+  res.render("login", { error: null });
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      console.log("No User with this email exists");
+      res.redirect("/register");
+    }
+
+    if (user.password !== password) {
+      console.log("Invalid password");
+      res.redirect("/");
+    }
+    res.render("compose");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+});
+
+app.get("/register", (req, res) => {
+  res.render("register", { error: null });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const newUser = await User.create({
+      email: email,
+      password: password,
+    });
+
+    console.log(newUser);
+
+    res.render("compose");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+});
+
+/* ---------- listen ---------- */
+app.listen(PORT, () => {
+  console.log(`Inkwell running on http://localhost:${PORT}`);
+});
