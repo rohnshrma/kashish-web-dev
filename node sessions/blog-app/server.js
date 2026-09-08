249f2a532a821a5adfde7277f320b773bb2ba2ff
rohnshrma <rohn11shrma@gmail.com>
2026-09-08 18:34:26 +0530

    feat: implement user authentication with Passport.js and session management

--- node sessions/blog-app/server.js
+++ node sessions/blog-app/server.js
@@ -4,6 +4,9 @@ import connectDB from "./config/db.js";
 import User from "./models/User.js";
 import { config } from "dotenv";
 import bcrypt from "bcryptjs";
+import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 
 config();
 connectDB();
@@ -14,6 +17,65 @@ const PORT = process.env.PORT || 3003;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return done(null, false, {
+            message: "No user with this email exists",
+          });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+          return done(null, false, { message: "Invalid password" });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function ensureAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 /* ---------- data (in memory for now) ---------- */
 const posts = [];
@@ -28,9 +90,9 @@ app.get("/blogs", (req, res) => {
   res.render("blogs", { posts: [...posts].reverse() });
 });
 
-// app.get("/compose", (req, res) => {
-//   res.render("compose", { error: null });
-// });
+app.get("/compose", ensureAuth, (req, res) => {
+  res.render("compose", { error: null });
+});
 
 app.post("/compose", (req, res) => {
   const { title, author, content } = req.body;
@@ -43,26 +105,13 @@ app.get("/login", (req, res) => {
   res.render("login", { error: null });
 });
 
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      console.log("No User with this email exists");
-      res.redirect("/register");
-    }
-
-    if (!(await bcrypt.compare(password, user.password))) {
-      console.log("Invalid password");
-      res.redirect("/login");
-    }
-    res.render("compose");
-  } catch (err) {
-    console.log(err);
-    res.redirect("/register");
-  }
-});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/compose",
+    failureRedirect: "/login",
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register", { error: null });
@@ -79,7 +128,7 @@ app.post("/register", async (req, res) => {
 
     console.log(newUser);
 
-    res.render("compose");
+    res.redirect("login");
   } catch (err) {
     console.log(err);
     res.redirect("/register");
